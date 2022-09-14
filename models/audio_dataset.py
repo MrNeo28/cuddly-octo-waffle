8ee0e73d93fd172df9e849d8c3b660482bc1a8bb
Ashish Vishwakarma <av.vishwakarma@hotmail.com>
2022-09-14 22:20:12 +0530

    added requirements.txt

--- models/audio_dataset.py
+++ models/audio_dataset.py
@@ -3,13 +3,13 @@ import torch.utils.data as torchdata
 
 from audiomentations import Compose, AddGaussianNoise, TimeStretch, PitchShift, Shift
 
-
 augment = Compose([
     AddGaussianNoise(min_amplitude=0.001, max_amplitude=0.015, p=0.5),
     TimeStretch(min_rate=0.8, max_rate=1.25, p=0.5),
     PitchShift(min_semitones=-4, max_semitones=4, p=0.5),
     Shift(min_fraction=-0.5, max_fraction=0.5, p=0.5),
 ])
+
 
 class WaveformDataset(torchdata.Dataset):
     def __init__(self,
@@ -20,18 +20,17 @@ class WaveformDataset(torchdata.Dataset):
 
     def __len__(self):
         return len(self.df)
-    
 
     def __getitem__(self, idx: int):
         sample = self.df.loc[idx, :]
-        
+
         wav_path = sample["file_path"]
         labels = sample["primary_label"]
-        
+
         image = np.load(wav_path)
         if mode == "train":
             image = augment(image, sample_rate=config.sample_rate)
-            
+
         targets = np.zeros(len(config.target_columns), dtype=float)
         for ebird_code in labels.split():
             targets[config.target_columns.index(ebird_code)] = 1.0
